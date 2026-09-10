00dba807a5ed30cf7bca713fe7ab03e0779c4d6d
Martin Fitzek <m4rfitzek@gmail.com>
2026-09-10 18:40:17 +0200

    move esp stuff to own folder, use platformio

---
+++ board/src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+
+#define SOLENOID_PIN 4
+#define PULSE_DURATION_MS 100
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(SOLENOID_PIN, OUTPUT);
+  digitalWrite(SOLENOID_PIN, LOW);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char c = Serial.read();
+
+    if (c == '1') {
+      digitalWrite(SOLENOID_PIN, HIGH);
+      delay(PULSE_DURATION_MS);
+      digitalWrite(SOLENOID_PIN, LOW);
+      delay(PULSE_DURATION_MS);
+      digitalWrite(SOLENOID_PIN, HIGH);
+      delay(PULSE_DURATION_MS);
+      digitalWrite(SOLENOID_PIN, LOW);
+      Serial.println("TUK");
+    }
+  }
+}
